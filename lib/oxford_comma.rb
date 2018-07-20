c54c5bac37fdbee1821b66b39c72555d65e474c0
Mika88 <mikaspivak@gmail.com>
2018-07-20 18:53:20 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length == 1
-    result = array[0] 
-  else 
+    result = array[0]
+  else
     results = "#{array[0, array.length].join(" , ")} and #{array.last}"
   end
 end
