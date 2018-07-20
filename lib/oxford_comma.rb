8845b7eddf6465ebc714b50dd324f20b765c4ea1
Mika88 <mikaspivak@gmail.com>
2018-07-20 18:53:17 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
 def oxford_comma(array)
-  array.to_sentence
+  if array.length == 1
+    result = array[0] 
+  else 
+    results = "#{array[0, array.length].join(" , ")} and #{array.last}"
+  end
 end
